160cd37c90c4c0d384cf4d9d633d2c0ea6d0a283
zahoorz <zahoor.zafrulla@gmail.com>
2021-02-17 21:18:58 +0000

    update stats module function to compute counts loss weight

--- basepairmodels/common/stats.py
+++ basepairmodels/common/stats.py
@@ -12,7 +12,7 @@ import numpy as np
 from mseqgen.quietexception import QuietException
 
 
-def get_recommended_counts_loss_weight(input_bigwigs, training_intervals, 
+def get_recommended_counts_loss_weight(input_bigWigs, peaks, 
                                        alpha=1.0):
     """
         This function computes the hyper parameter lambda (l) as
@@ -29,13 +29,13 @@ def get_recommended_counts_loss_weight(input_bigwigs, training_intervals,
         l = (alpha / 2) * n_obs
     
         Args:
-            input_bigwigs (list): list of bigwig files with assay
+            input_bigWigs (list): list of bigWig files with assay
                 signal. n_obs will computed as a global average
-                across all the input bigwigs
+                across all the input bigWigs
             
-            training_intervals (pandas.Dataframe): 3 column dataframe  
-                with 'chrom', 'start' and 'end' columns, representing 
-                range [start, end) for the spans of interest 
+            peaks (list): list 3 column pandas dataframes,
+                 with 'chrom', 'start' and 'end' columns,
+                 corresponding to each input bigWig
 
             alpha (float): parameter to scale profile loss relative
                 to the counts loss. A value < 1.0 will upweight the
@@ -46,37 +46,45 @@ def get_recommended_counts_loss_weight(input_bigwigs, training_intervals,
             
     """
 
+    # check if 'input_bigwigs' and 'peaks' have the same length
+    if len(input_bigWigs) != len(peaks):
+        raise QuietException("There should be same number of peaks "
+                             "dataframes as input bigWigs") 
+    
     # check to make sure all bigwigs are valid files
-    for bigwig in input_bigwigs:
-        if not os.path.exists(bigwig):
-            raise QuietException("File {} does not exist".format(bigwig))
+    for bigWig in input_bigWigs:
+        if not os.path.exists(bigWig):
+            raise QuietException("File {} does not exist".format(bigWig))
 
     # open each bigwig and add file pointers to a list
-    bigwigs = []
-    for bigwig in input_bigwigs:
-        bigwigs.append(pyBigWig.open(bigwig))
+    bigWigs = []
+    for bigWig in input_bigWigs:
+        bigWigs.append(pyBigWig.open(bigWig))
     
     # total counts from all training windows across all bigwigs
     total_counts = 0
     
-    # iterate over training windows
-    for _idx, row in training_intervals.iterrows():
+    # get the total counts
+    total_peaks = 0
+    for i in range(len(bigWigs)):
+        bw = bigWigs[i]
         
-        # chrom window
-        chrom = row['chrom']
-        start = row['start']
-        end = row['end']
-        # iterate over bigwigs for each window and accumulate the sum
-        for bw in bigwigs:
-            total_counts += np.sum(np.nan_to_num(bw.values(chrom, start, end)))
+        # iterate over all the corresponding peaks
+        for _idx, row in peaks[i].iterrows():
+            # chrom window
+            chrom = row['chrom']
+            start = row['start']
+            end = row['end']
+            total_counts += np.sum(
+                np.nan_to_num(bw.values(chrom, start, end)))
+        
+        total_peaks += peaks[i].shape[0]
         
     # average of the total counts
-    n_obs = total_counts / (len(bigwigs) * len(training_intervals))
+    n_obs = total_counts / float(total_peaks)
     
-    return (alpha / 2) * n_obs
+    return (alpha / 2.0) * n_obs
 
-
-            
             
             
         
