4abb940704f255e332c07a0f8173f888f430d032
zahoorz <zahoor.zafrulla@gmail.com>
2021-05-19 01:20:40 +0000

    updates to attribution prior model

--- basepairmodels/common/attribution_prior.py
+++ basepairmodels/common/attribution_prior.py
@@ -1,10 +1,15 @@
+import tensorflow as tf
+import tensorflow.keras.backend as kb
+
+from basepairmodels.cli.losses import fourier_att_prior_loss
 from tensorflow.keras import Model
+from tensorflow.nn import log_softmax
 
 class AttributionPriorModel(Model):
 
     def __init__(self, frequency_limit, limit_softness, grad_smooth_sigma, 
                  **kwargs):
-        super(CustomModel, self).__init__(**kwargs)
+        super(AttributionPriorModel, self).__init__(**kwargs)
         
         self.freq_limit = frequency_limit
         self.limit_softness = limit_softness
@@ -16,7 +21,7 @@ class AttributionPriorModel(Model):
         x, y = data
 
         with tf.GradientTape() as tape:
-            with tf.GradientTape() as input_grad_tape:
+            with tf.GradientTape(persistent=True) as input_grad_tape:
                 input_grad_tape.watch(x)
                 
                 y_pred = self(x, training=True)  # Forward pass
@@ -25,42 +30,42 @@ class AttributionPriorModel(Model):
                 batch_loss = self.compiled_loss(
                     y, y_pred, regularization_losses=self.losses)
 
-            if self.use_prior:
-                
-                # mean-normalize the profile logits output & weight by
-                # post-softmax probabilities
-                y_pred_profile = y_pred[0] - kb.mean(y_pred[0])
-                y_pred_profile *= log_softmax(y_pred_profile)
-                
-                # Compute gradients of the output with respect to the input
-                
-                # gradients of profile output w.r.t to input
-                input_grads_profile = input_grad_tape.gradient(
-                    y_pred_profile, x['sequence'])
-                # gradients of counts output w.r.t to input
-                input_grads_counts = input_grad_tape.gradient(
-                    y_pred[1], x['sequence'])
-                
-                # Gradient * input
-                input_grads_profile = input_grads_profile * x['sequence']  
-                input_grads_counts = input_grads_counts * x['sequence']  
-                
-                # attribution prior loss of profile
-                batch_attr_prior_loss_profile = self.fourier_att_prior_loss(
-                    x['sequence'], input_grads_profile, self.freq_limit, 
-                    self.limit_softness,
-                    self.att_prior_grad_smooth_sigma
-                )
+            # mean-normalize the profile logits output & weight by
+            # post-softmax probabilities
+            y_pred_profile = y_pred[0] - kb.mean(y_pred[0])
+            y_pred_profile *= log_softmax(y_pred_profile)
 
-                # attribution prior loss of counts
-                batch_attr_prior_loss_counts = self.fourier_att_prior_loss(
-                    x['sequence'], input_grads_counts, self.freq_limit, 
-                    self.limit_softness,
-                    self.att_prior_grad_smooth_sigma
-                )
+            # Compute gradients of the output with respect to the input
 
-                batch_loss = batch_loss + batch_attr_prior_loss_profile + \
-                    batch_attr_prior_loss_counts
+            # gradients of profile output w.r.t to input
+            input_grads_profile = input_grad_tape.gradient(
+                y_pred[0], x['sequence'])
+            # gradients of counts output w.r.t to input
+            input_grads_counts = input_grad_tape.gradient(
+                y_pred[1], x['sequence'])
+            
+            # Gradient * input
+            input_grads_profile = input_grads_profile * x['sequence']  
+            input_grads_counts = input_grads_counts * x['sequence']  
+
+            # attribution prior loss of profile
+            batch_attr_prior_loss_profile = fourier_att_prior_loss(
+                x['status'], input_grads_profile, self.freq_limit, 
+                self.limit_softness, self.grad_smooth_sigma)
+
+            # attribution prior loss of counts
+            batch_attr_prior_loss_counts = fourier_att_prior_loss(
+                x['status'], input_grads_counts, self.freq_limit, 
+                self.limit_softness, self.grad_smooth_sigma)
+
+            batch_attr_prior_loss = batch_attr_prior_loss_profile + \
+                batch_attr_prior_loss_counts
+            
+            batch_loss = batch_loss + batch_attr_prior_loss
+
+            # delete the gradient tape because we made it persistent 
+            # to compute gradients more than once 
+            del input_grad_tape
 
         # Compute gradients
         trainable_vars = self.trainable_variables
@@ -75,7 +80,6 @@ class AttributionPriorModel(Model):
         # Return a dict mapping metric names to current value
         metrics = {m.name: m.result() for m in self.metrics}
                 
-        if self.use_prior:
-            metrics['batch_attribution_prior_loss'] = attribution_prior_loss
+        metrics['attribution_prior_loss'] = batch_attr_prior_loss
         
         return metrics
