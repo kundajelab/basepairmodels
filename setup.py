e1120f1939e2ed0a3758527049b8831ce81bdeca
zahoorz <zahoor.zafrulla@gmail.com>
2022-05-05 01:33:16 +0000

    update version for pre release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="basepairmodels",
-    version='0.2.1',
+    version='0.3.0',
     description=("BPNet: toolkit to learn motif synthax from high-resolution functional genomics data"
                  " using convolutional neural networks"),
     author="Zahoor Zafrulla",
